e3d48b4fd31d9759b432bade3efb4a9f89ccbae1
Yang Zhao <freda.y.zhao@gmail.com>
2022-08-01 14:24:46 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<h1>{{title}}</h1>
+<br>
+<input type="text" ref="name">
+<button @click='handleClick'>Click me</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+data(){
+  return{
+    title: 'My First Vue Project'
+  }
+},
+methods: {
+  handleClick(){
+    console.log(this.$refs.name)
+    this.$refs.name.classList.add('active')
+    // mouse focus on input field
+    this.$refs.name.focus()
+  }
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
